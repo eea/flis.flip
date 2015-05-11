3ff32d8f28b9854251df72c91f610312883d9ef2
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-05-11 16:46:22 +0300

    #25420 added a blossom logo removed harcoding of hostname

--- flip/settings.py
+++ flip/settings.py
@@ -77,6 +77,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
+    'flip.context_processors.site',
 )
 
 ROOT_URLCONF = 'flip.urls'
